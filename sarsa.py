c0fe768f540e0db1f070ecc9d58b6c4cbd306f49
James Martindale <11380394+jkmartindale@users.noreply.github.com>
2020-10-10 18:49:53 -0500

    Make discount factor actually sane
    Also  
    - render toggle (when turned off, learning machine go brrrrrrrrrrrrrrrrrrr)  
    - Custom episode length, as a treat

--- sarsa.py
+++ sarsa.py
@@ -4,24 +4,34 @@ import numpy
 ############
 # Parameters
 ############
-discount_factor = 0.05
+discount_factor = 0.9
 exploration_chance = 0.05
+render = False
 step_size = 0.5
-steps_per_episode = 200
-training_episodes = 100
+# Originally 200
+steps_per_episode = 300
+training_episodes = 1000
 # Tile configuration
 position_levels = 20
 velocity_levels = 20
+
+# Override episode length
+gym.envs.register(
+    id='MountainCar-CustomLength-v0',
+    entry_point='gym.envs.classic_control:MountainCarEnv',
+    max_episode_steps=steps_per_episode,
+    reward_threshold=-110.0,
+)
 
 actions = [0, 1, 2]
 # Q[tile, action]
 Q = [[0 for _ in actions] for _ in range(position_levels*velocity_levels)] # Optimistic for now
 
-env = gym.make('MountainCar-v0')
+env = gym.make('MountainCar-CustomLength-v0')
 
 position_bins = numpy.linspace(env.observation_space.low[0], env.observation_space.high[0], position_levels)
 velocity_bins = numpy.linspace(env.observation_space.low[1], env.observation_space.high[1], velocity_levels)
-def discretize(observation: numpy.ndarray) -> numpy.int64:
+def tile(observation: numpy.ndarray) -> numpy.int64:
     """Convert a continuous position and velocity to a discrete tile."""
     return numpy.digitize(observation[0], position_bins) * velocity_levels + numpy.digitize(observation[1], velocity_bins)
 
@@ -38,7 +48,7 @@ def policy(state: int) -> numpy.int64:
 for episode in range(training_episodes):
     # Initialize S
     observation = env.reset()
-    state = discretize(observation)
+    state = tile(observation)
 
     # Choose A from S using policy derived from Q
     action = policy(state)
@@ -46,14 +56,15 @@ for episode in range(training_episodes):
     # Loop for each step of episode:
     t = 0
     total_reward = 0
-    for _ in range(steps_per_episode):
+    done = False
+    while not done:
         # Take action A, observe R, S'
         t += 1
         observation, reward, done, info = env.step(action)
-        env.render()
-        if done:
-            break
-        next_state = discretize(observation)
+        if render:
+        #if episode > 200:
+            env.render()
+        next_state = tile(observation)
         total_reward += reward
 
         # Choose A' from S' using policy derived from Q
